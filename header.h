20e5e96299590f45ec294df6f77b3b46615d6fae
ther <1990592590@qq.com>
2025-05-20 16:41:41 +0800

    commit2

--- header.h
+++ header.h
@@ -1,1 +1,13 @@
-#pragma once
+// Windows 头文件
+#include <windows.h>
+#include<mmsystem.h>       //系统多媒体头文件 
+#pragma comment(lib,"winmm.lib")						//导入库文件
+// C 运行时头文件
+#include <iostream>
+#include<vector>
+#include <malloc.h>
+#include <memory.h>
+#include <string.h>
+#include <graphics.h>
+#include<conio.h>
+using namespace std;
