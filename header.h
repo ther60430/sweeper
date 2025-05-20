7b12d5d13aafd7e3df183e622f77c6f3711e2c59
ther <1990592590@qq.com>
2025-05-20 17:25:32 +0800

    5

--- header.h
+++ header.h
@@ -2,9 +2,9 @@
 #define HEADER_H
 // Windows 
 #include <windows.h>
-#include <mmsystem.h>             //六百六十六
-#pragma comment(lib, "winmm.lib") // ������ļ�
-// C ����ʱͷ�ļ�
+#include <mmsystem.h>             //外部媒体库
+#pragma comment(lib, "winmm.lib")  
+// C ++运行文件
 #include <iostream>
 #include <vector>
 #include <malloc.h>
