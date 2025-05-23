7e672cc74afa849afac5a476a1f6eedcc46e91c0
ther <1990592590@qq.com>
2025-05-23 16:51:37 +0800

    1

--- main.cpp
+++ main.cpp
@@ -7,11 +7,10 @@ struct blank
 
 
 IMAGE UnCell;         //未揭开格子图片
-IMAGE Cell;             //已揭开格子图片
+IMAGE Cell;             //已揭开格子图片              
 IMAGE HoverCell;   //鼠标悬停格子图片
 IMAGE Landmine;   //雷图片
 IMAGE Flag;            //旗帜图片
-IMAGE Background; //背景图片
 IMAGE GameOver; //游戏结束图片
 IMAGE Win;            //游戏胜利图片
 IMAGE GameStart;  //游戏开始图片
@@ -19,13 +18,12 @@ IMAGE GamePause;  //游戏暂停图片
 IMAGE GameContinue; //继续游戏图片
 IMAGE GameRestart;  //重新开始图片
 IMAGE GameExit;    //退出游戏图片
-IMAGE num[9];     //数字图片0~9
+IMAGE num[8];     //数字图片1-8
 
 class settings
 {
 public:
-	int width = 1200, height = 600;//屏幕宽高
-
+	int width = 1200, height = 600;     //屏幕宽高
 	COLORREF  Backcolor =RGB(57,197,187);
 };
 
@@ -38,7 +36,6 @@ class SweeperGame
 		bool first_click;                     //是否第一次点击
 		settings setting;
 	public:
-		void CreateWindows();              //创建窗口
 		void InitGame();                        //初始化游戏
 		void HandleInput();                  //处理输入
 		void reveal(int x, int y);             //揭开格子
@@ -53,6 +50,31 @@ class SweeperGame
 				_getch();
 		}
 };
+
+void SweeperGame::InitGame()
+{
+	initgraph(setting.width, setting.height);
+	setbkcolor(setting.Backcolor);
+	loadimage(&UnCell, _T("images/.png"));          //未揭开格子图片
+	loadimage(&Cell, _T("images/0.png"));            //已揭开格子图片
+	loadimage(&Landmine, _T("images/thounder.png"));              //雷图片
+	loadimage(&GameStart, _T("images/start.png"));              //游戏开始图片
+	loadimage(&GameOver, _T("images/.png"));                    //游戏结束图片
+	loadimage(&GamePause, _T("images/pause.png"));           //游戏暂停图片
+	loadimage(&GameContinue, _T("images/.png"));            //继续游戏图片
+	loadimage(&GameRestart, _T("images/.png"));                   //重新开始图片
+	loadimage(&GameExit, _T("images/end.png"));               //退出游戏按钮图片
+	loadimage(&Flag, _T("images/flag.png"));             //旗帜图片
+	loadimage(&num[0], _T("images/1.png"));
+	loadimage(&num[1], _T("images/2.png"));
+	loadimage(&num[2], _T("images/3.png"));
+	loadimage(&num[3], _T("images/4.png"));
+	loadimage(&num[4], _T("images/5.png"));
+	loadimage(&num[5], _T("images/6.png"));
+	loadimage(&num[6], _T("images/7.png"));
+	loadimage(&num[7], _T("images/8.png"));
+
+}
 
 int main()
 {
